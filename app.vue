b61545d0a0c8d3102ce622204eb14cbf3aa10e84
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-09-25 22:53:18 +0300

    *

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <ul>
+      <li v-for="post in posts" :key="posts.id">
+        {{ user.name }}
+      </li>
+    </ul> -->
+    wewe
+  </div>
+</template>
+
+<script setup>
+const client = useSupabaseClient();
+// const { data, error } = await useAsyncData('tests', async () => {
+//   const { data } = await client.from('test').select();
+//   console.log(data);
+//   return data;
+// });
+
+let { data, error } = await client.from('test').select('*');
+
+// console.log(data);
+console.log(data);
+console.log(error);
+</script>
